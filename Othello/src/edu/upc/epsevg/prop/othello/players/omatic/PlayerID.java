ec6dcf651190a09cd7bf04f36f0ac4a7065aa871
icorn1 <ixent.cornella@estudiantat.upc.edu>
2022-11-25 10:03:49 +0100

    Inicialització Classes
    Petita inicialització de la classe PlayerMiniMax, així com la creació del package al que pertany, edu.upc.epsevg.prop.othello.players.omatic.

---
+++ Othello/src/edu/upc/epsevg/prop/othello/players/omatic/PlayerID.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.omatic;
+
+/**
+ *
+ * @author icorn1, eric_gonsalez
+ */
+public class PlayerID {
+    
+}
